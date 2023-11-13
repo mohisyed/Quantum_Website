b81c336e26c419bbea824834f648e5df65c32014
Sandhya Khandagale <92696801+sandhyakhand@users.noreply.github.com>
2023-11-13 11:24:35 -0500

    Update signup.php

--- signup.php
+++ signup.php
@@ -13,40 +13,45 @@
         //will be connected to database...
         $db = mysqli_connect("studentdb-maria.gl.umbc.edu","skhanda1","skhanda1","skhanda1");
         if (mysqli_connect_errno()){
-        exit("Error - could not connect to mySQL");}
+        exit("Error - could not connect to MySQL");}
 
         //hmtl injection
         $firstName = htmlspecialchars($_POST["firstName"]);
         $lastName = htmlspecialchars($_POST["lastName"]);
         $email = htmlspecialchars($_POST["email"]);
-        $password = htmlspecialchars($_POST["password"]);
         $campusID = htmlspecialchars($_POST["campusID"]);
+        $username = htmlspecialchars($_POST["username"]);
+        $password = htmlspecialchars($_POST["password"]);
 
         //mysql injection
         $firstName = mysqli_real_escape_string($db, $firstName);
         $lastName = mysqli_real_escape_string($db, $lastName);
         $email = mysqli_real_escape_string($db, $email);
-        $password = mysqli_real_escape_string($db,$password);
         $campusID = mysqli_real_escape_string($db, $campusID);
+        $username = mysqli_real_escape_string($db, $username);
+        $password = mysqli_real_escape_string($db,$password);
+
 
         //print statements
         echo "First Name: $firstName <br>";
         echo "Last Name: $lastName <br>";
         echo "Email: $email <br>";
-        echo "Password: $password <br>";
         echo "Campus ID: $campusID <br>";
 
         //sql query
-        $constructed_query = "INSERT INTO newUser(firstName, lastName, email, password; campusID) VALUES('$firstName', '$lastName','$email','$password', '$campusID')";
+        $constructed_query = "INSERT INTO newUser(firstName, lastName,  email, campusID, username, password) VALUES('$firstName', '$lastName','$email','campusID','username','$password')";
 
         //execute SQL squery
-        $result = mysqli_query($db, $constructed_query);
+        mysqli_query($db, $constructed_query);
 
-        //will be linked to join team page
-        //echo "<a href='dashboard.html'>Team Page</a>";
+        if(mysqli_error($db)) {
+          die("Error: " .mysqli_error($db));
+        }
+        else {
+          echo "New User Created";
+        }
+
         ?>
-
-    <p>New User Created</p>
     <br>
     <br>
     <div class = "button-center">
