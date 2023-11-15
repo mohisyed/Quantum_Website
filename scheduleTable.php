fc7bed698ef81a3ab7a8c83858d5aa9dee63bded
Mohi <67804307+mohisyed@users.noreply.github.com>
2023-11-15 14:30:25 -0500

    Create scheduleTable.php
    table that displays the schedule

---
+++ scheduleTable.php
@@ -0,0 +1,48 @@
+
+
+
+
+<?php
+    $db = mysqli_connect("studentdb-maria.gl.umbc.edu", "dabimbo1", "dabimbo1", "dabimbo1");
+
+    $sql = "SELECT dates, HOME, AWAY FROM schedule";
+    $result = $conn->query($sql);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Schedule</title>
+</head>
+<body>
+
+<table border="1">
+    <tr>
+        <th>Date</th>
+        <th>Home Team</th>
+        <th>Away Team</th>
+    </tr>
+    
+    <?php
+    // Loop through the results and populate the table
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row['Date'] . "</td>";
+        echo "<td>" . $row['HOME'] . "</td>";
+        echo "<td>" . $row['AWAY'] . "</td>";
+        echo "</tr>";
+    }
+    
+    // Close the result set and connection
+    $result->close();
+    $conn->close();
+    ?>
+</table>
+
+</body>
+</html>
+
